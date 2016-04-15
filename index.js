0b4615a0f170f37a30af3efe98ce575c3c3a7868
Karlis Upitis <karlis@upitis.com>
2016-04-15 17:33:41 +0200

    switching from looping through files to looping through folders

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 // load dependencies
 var fs = require('fs'),						//filesystem
 	path = require('path'),					//paths
-	recursive = require('recursive-readdir');
+	recursive = require('recursive-readdir'),
+	escape = require('escape-html');
+
 
 
 module.exports = function(){
@@ -11,7 +13,9 @@ module.exports = function(){
 	var tmplElem = 'src/styleguide/source/element.template';
 	var resultUrl = 'src/styleguide/index.nunjucks';
 	var plhdLoop = '[[[styleguide-placeholder]]]';
-	var plhdFileUrl = "[[[file-url]]]";
+	var plhdFileUrl = "/\[\[\[file-url]]]/g";
+	var plhdFileSrc = "/\[\[\[file-source]]]/g";
+	var plhdFileMD  = "/\[\[\[file-description]]]/g";
 	var content = 'There are no components in components folder';
 	// find content placeholder
 	// replace it with includes of the files
@@ -23,67 +27,48 @@ module.exports = function(){
 	var contentsElem = fs.readFileSync(tmplElem).toString();
 
 	// -------------------------------------
-	//  get all components
+	//  V1 Going through files
 	// -------------------------------------
-	recursive('src/components',['*.json', '*.scss', '*.md'] , function (err, files) {
-		var count = files.length;
-		if(count===0) return;
-		content = '';
-		for(var i=0; i<count; i++){
-			// var re = new RegExp(plhdFileUrl,"g");
-			// console.log(re);
-			content += contentsElem.replace(/\[\[\[file-url]]]/g, files[i].replace(/\\/g,"/"));
+	// recursive('src/components',['*.json', '*.scss', '*.md'] , function (err, files) {
+	// 	var count = files.length;
+	// 	if(count===0) return;
+	// 	content = '';
+	// 	for(var i=0; i<count; i++){
+	// 		//tmp solution (doesn't regenerate on template modifications)
+	// 		var sourceCode = fs.readFileSync(files[i],'utf8');
+	// 		content += contentsElem
+	// 			.replace(plhdFileUrl, files[i].replace(/\\/g,"/"))
+	// 			.replace(plhdFileSrc, escape(sourceCode))
+	// 			.replace(plhdFileMD , escape(sourceCode));
+	// 	}
+	// 	contentsPage = contentsPage.replace(plhdLoop, content); 
+	// 	// -------------------------------------
+	// 	//  write files
+	// 	// -------------------------------------
+	// 	fs.writeFileSync(resultUrl, contentsPage);
+	// });
 
-		}
-		contentsPage = contentsPage.replace(plhdLoop, content); 
-		// -------------------------------------
-		//  write files
-		// -------------------------------------
-		fs.writeFileSync(resultUrl, contentsPage);
-	});
+
+	// -------------------------------------
+	//  V2 Going through folders
+	// -------------------------------------
+	var srcpath = 'src/components';
+	function recursiveWalkTree(dir){
+		fs.readdirSync(dir).filter(function(file) {
+			// console.log(dir,file);
+			var dirPath = path.join(dir, file);
+			var isDirectory = fs.statSync(dirPath).isDirectory();
+			console.log(file, isDirectory);
+			if(isDirectory) recursiveWalkTree(dirPath);
+		// console.log(fs.statSync(path.join(srcpath, file)).isDirectory())
+		// return fs.statSync(path.join(srcpath, file)).isDirectory();
+		});
+	}
+	recursiveWalkTree(srcpath);
+
+
+
 }
-
-
-// function initPatterLibrary(files){
-// 	var tmplPage = 'src/styleguide/source/page.template';
-// 	var tmplElem = 'src/styleguide/source/element.template';
-// 	var resultUrl = 'src/styleguide/index.nunjucks';
-// 	var plhdLoop = '[[[styleguide-placeholder]]]';
-// 	var plhdFileUrl = "[[[file-url]]]";
-// 	var content = 'There are no components in components folder';
-// 	// find content placeholder
-// 	// replace it with includes of the files
-	 
-// 	// -------------------------------------
-// 	//  read files
-// 	// -------------------------------------
-// 	var contentsPage = fs.readFileSync(tmplPage).toString();
-// 	var contentsElem = fs.readFileSync(tmplElem).toString();
-
-// 	// -------------------------------------
-// 	//  get all components
-// 	// -------------------------------------
-// 	recursive('src/components',['*.json', '*.scss', '*.md'] , function (err, files) {
-// 		var count = files.length;
-// 		if(count===0) return;
-// 		content = '';
-// 		for(var i=0; i<count; i++){
-// 			// var re = new RegExp(plhdFileUrl,"g");
-// 			// console.log(re);
-// 			content += contentsElem.replace(/\[\[\[file-url]]]/g, files[i].replace(/\\/g,"/"));
-
-// 		}
-// 		contentsPage = contentsPage.replace(plhdLoop, content); 
-// 		// -------------------------------------
-// 		//  write files
-// 		// -------------------------------------
-// 		fs.writeFileSync(resultUrl, contentsPage);
-// 	});
-	
-// }
-
-// initPatterLibrary();
-
 
 
 
