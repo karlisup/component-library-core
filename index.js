f18ca014784ab4fedb65af380400fd7e8141b038
karlis upitis <karlis@upitis.com>
2016-04-18 08:45:16 +0200

    added dependencies - escape-html, better console. Tree structure to JSON function

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 var fs = require('fs'),						//filesystem
 	path = require('path'),					//paths
 	recursive = require('recursive-readdir'),
-	escape = require('escape-html');
+	escape = require('escape-html'),
+	console = require('better-console');
 
 
 
@@ -53,27 +54,90 @@ module.exports = function(){
 	//  V2 Going through folders
 	// -------------------------------------
 	var srcpath = 'src/components';
-	function recursiveWalkTree(dir){
+	var fileTree = [];
+
+	// var filteredTree = directoryTree('/some/path', ['.jpg', '.png']);
+	/**
+	 * Recursive function which walks the file-tree and saves it in to the JSON
+	 * @param  {string} dir - recursion starting directory
+	 * @param  {array} accepted - array of accepted file types (otherwise, ignore is more complicated)
+	 * @return {JSON}  
+	 */
+	function recursiveWalkTree(dir, accepted){
+		var output = [];
 		fs.readdirSync(dir).filter(function(file) {
-			// console.log(dir,file);
-			var dirPath = path.join(dir, file);
-			var isDirectory = fs.statSync(dirPath).isDirectory();
-			console.log(file, isDirectory);
-			if(isDirectory) recursiveWalkTree(dirPath);
-		// console.log(fs.statSync(path.join(srcpath, file)).isDirectory())
-		// return fs.statSync(path.join(srcpath, file)).isDirectory();
+			// console.log(a);
+			var item = {};
+			item.name = file;
+			item.path = path.join(dir, file);
+
+			var isDirectory = fs.statSync(item.path).isDirectory();
+			if(isDirectory){
+				// if directory call function on new path
+				item.children = recursiveWalkTree(item.path);
+				console.table(item.children);
+			}
+			else{
+				// if file, make sure it's whitelisted
+				// http://blog.niftysnippets.org/2010/09/say-what.html
+				var what = Object.prototype.toString;
+				if(what.call(accepted) == "[object Array]"){
+					//geez
+				}
+				elseif(what.call(accepted) == "[object String]"){
+					//geez
+				}
+			}
+			output.push(item);
 		});
+		return output;
 	}
-	recursiveWalkTree(srcpath);
+
+	console.info("Save Directory Tree in JSON");
+	console.time("Timer");
+	fileTree = recursiveWalkTree(srcpath, ['.md', '.']);
+	console.table(fileTree);
+	console.timeEnd("Timer");
+	// console.log(fileTree);
 
 
+
+// }
+// photos
+// ├── summer
+// │   └── june
+// │       └── windsurf.jpg
+// └── winter
+//     └── january
+//         ├── ski.png
+//         └── snowboard.jpg
+
+
+// {
+//   "path": "",
+//   "name": "photos",
+//   "type": "directory",
+//   "children": [
+//     {
+//       "path": "winter/january",
+//       "name": "january",
+//       "type": "directory",
+//       "children": [
+//         {
+//           "path": "winter/january/ski.png",
+//           "name": "ski.png",
+//           "type": "file"
+//         },
+//         {
+//           "path": "winter/january/snowboard.jpg",
+//           "name": "snowboard.jpg",
+//           "type": "file"
+//         }
+//       ]
+//     }
+//   ]
 
 }
-
-
-
-
-
 
 
 
