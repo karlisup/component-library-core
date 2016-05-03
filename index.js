bba3eae2c244a801669467c76c6b8edeca131d90
Karlis Upitis <karlis@upitis.com>
2016-05-03 12:18:35 +0200

    Alpha version of watch task. Works on delete, on create and on change

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const console = require('better-console');
 const Twig = require('twig');
 const twig = Twig.twig;						//twig render function
 const watch = require('watch');
+const rimraf = require('rimraf'); 			// remove folder
 // local dependencies
 const helpers = require('./helpers');
 
@@ -68,8 +69,7 @@ try {
 
 // recursive function
 function treeToJSON(dir, buildComponentsList){
-   buildComponentsList = typeof buildComponentsList !== 'undefined' ? buildComponentsList : false;
-   console.log('buildCompList'+buildComponentsList);
+	buildComponentsList = typeof buildComponentsList !== 'undefined' ? buildComponentsList : false;
 	var output = [];				// variable which later will be returned.
 	// var re = /(?:\.([^.]+))?$/; 	// get extension regexp TODO:deprecated
 
@@ -109,7 +109,7 @@ function treeToJSON(dir, buildComponentsList){
 		var isDirectory = fs.statSync(item.path).isDirectory();
 		if(isDirectory){
 			// if directory call function on new path
-			item.children = treeToJSON(item.path);
+			item.children = treeToJSON(item.path, buildComponentsList);
 			whitelisted = true;
 		}
 		else{
@@ -129,6 +129,7 @@ function treeToJSON(dir, buildComponentsList){
 }
 
 function assembleComponent(component) {
+	console.info('Assembling component: '+component.name);
 	// var re = /(?:\.([^.]+))?$/; 	// get extension regexp TODO:deprecated
 	fs.readdirSync(location.src+component.path).filter(function(file){
 		var extension = (path.extname(file) == undefined)? 'folder': path.extname(file).toLowerCase();
@@ -144,6 +145,7 @@ function assembleComponent(component) {
 		}
 	});
 	components.push(component);
+	return component;
 }
 
 
@@ -170,61 +172,34 @@ function build(){
 }
 
 
-// -------------------------------------
-//  Watch task
-// -------------------------------------
-watch.createMonitor(location.src, function (monitor) {
-	monitor.on("created", function (filePath, stat) {
-		console.log('created', filePath);
-		// if new template --> assembleComponent
-		// if new file with same name as template - update .json(s)
-		var extension = path.extname(filePath).toLowerCase();
-		if(extension === extensions.template){
-			var component = new Component(filePath);
-			assembleComponent(component);
-		}
-		else if(extension === ''){} // folder
-		else{
-			var component = componentExist(filePath);
-			if(component){
-				componentUpdate(component, filePath);
-				componentRender(component);
-			}
-		}
-
-
-		console.log(component);
-		console.table(components);
-	})
-	monitor.on("changed", function (f, curr, prev) {
-		console.log('changed', f);
-		// Handle file changes
-	})
-	monitor.on("removed", function (f, stat) {
-		console.log('removed', f);
-		// Handle removed files
-	})
-// monitor.stop(); // Stop watching
-})
 
 
 // -------------------------------------
 //  component related functions
 // -------------------------------------
 function componentRender(component){
-	var templateData = '';
+	console.info('Rendering component: '+component.name);
+	var templateData;
 	var	templateContents = fs.readFileSync(location.src+component.tmpl).toString();
 	if(component.data !== ''){
-		templateData = fs.readFileSync(location.src+component.data).toString();
+		try{
+			templateData = JSON.parse(fs.readFileSync(path.join(location.src, component.data), 'utf8'));
+		}catch(e){
+			console.error(e);
+		}
 	}
 	
 	// compile template with given data
 	var template = twig({ data: templateContents});
-	var output = template.render({ templateData });
-	component.compiled = component.path+component.name+extensions.output;
+	var output = template.render(templateData);
+	component.compiled = path.join(component.path, component.name+extensions.output);
 
 	// create file
-	helpers.writeFile(location.dest+components[key].compiled, output);
+	helpers.writeFile(path.join(location.dest, component.compiled), output, function(err){
+		if(err){
+			console.error(err);
+		}
+	});
 }
 
 function componentExist(filePath){
@@ -233,7 +208,7 @@ function componentExist(filePath){
 	var compPath = path.dirname(filePath).split(location.src)[1]; // relative folder url
 	var compExtension = path.extname(filePath).toLowerCase();
 	var compName = path.basename(filePath, compExtension); ///foo/bar/baz/asdf/quux.html ==> quux;
-	console.log(compPath, compName, compExtension);
+	// console.log(compPath, compName, compExtension);
 	for (var key in components) {
 		if (components.hasOwnProperty(key)) {
 			if(components[key].path === compPath && components[key].name === compName){
@@ -244,21 +219,84 @@ function componentExist(filePath){
 	return false;
 }
 
-function componentUpdate(component, filePath){
+function componentUpdate(component, filePath, action){
+	console.info('Updating component: '+action+' '+component.name);
+	var extension = path.extname(filePath).toLowerCase();
+
 	if(['.md','.json'].indexOf(extension) > -1){
-		var relativeFileLocation = filePath.split(location.src)[1]; // relative file url
-		switch (extension) {
-			case '.json':
-			component.data = relativeFileLocation;
-			break;
-			case '.md':
-			component.info = relativeFileLocation;
-			break;
+
+		if(action==='add'){
+			var relativeFileLocation = filePath.split(location.src)[1]; // relative file url
+			switch (extension) {
+				case '.json':
+				component.data = relativeFileLocation;
+				break;
+				case '.md':
+				component.info = relativeFileLocation;
+				break;
+			}
+		}
+		else if(action==='rm'){
+			switch (extension) {
+				case '.json':
+				component.data = '';
+				break;
+				case '.md':
+				component.info = '';
+				break;
+			}
 		}
 		reloadTreeStructure()
 	}
 }
 
+function componentDelete(component){
+	console.warn('Deleting component: '+component.name+' from: '+component.path);
+	rimraf(path.join(location.dest, component.path) , function(err) {
+		if (err){
+			// throw err;
+			console.error('unable to delete component because: '+err);
+		}
+		else{
+			console.log('folder successfully removed, deleting component:'+ JSON.stringify(component, null, 4) );
+			// is this the best way to do it?
+			componentIndex = components.indexOf(component);
+			delete components[componentIndex]; // remove component from components array
+			reloadTreeStructure() // remove files from accessing from treeStructure array
+			console.table(components);
+		}
+	});
+}
+
+/**
+ * How many template files does component have?
+ * @param  {object} component
+ * @return {int}          
+ */
+function templatesInFolder(component){
+	var templateCount = 0;
+	fs.readdirSync(location.src+component.path).filter(function(file){
+		templateCount += (path.extname(file) == extensions.template)? 1 : 0;
+	});
+
+	return templateCount;
+}
+
+function templateDelete(component){
+	console.warn('Deleting template: '+component.tmpl);
+	var templateUrl = path.join(location.dest, component.compiled)
+	fs.exists(templateUrl, function(exists) {
+		if(exists) {
+			fs.unlinkSync(templateUrl);
+			componentIndex = components.indexOf(component);
+			delete components[componentIndex]; // remove component from components array
+			reloadTreeStructure() // remove files from accessing from treeStructure array
+			console.table(components);
+		} else {
+			console.warn('Trying to delete file witch doesn\'t exist');
+		}
+	});
+}
 
 
 
@@ -272,277 +310,72 @@ function reloadTreeStructure(){
 }
 
 
+// -------------------------------------
+//  Watch task
+// -------------------------------------
+watch.createMonitor(location.src, function (monitor) {
+	monitor.on("created", function (filePath, stat) {
+		console.warn('created: '+filePath);
+		// if new template --> assembleComponent
+		// if new file with same name as template - update .json(s)
+		var extension = path.extname(filePath).toLowerCase();
+		if(extension === extensions.template){
+			//TODO CHECK IF COMPONENT ALREADY EXISTS!
+			var component = new Component(filePath);
+			component = assembleComponent(component);
+			componentRender(component);
+		}
+		else if(extension === ''){} // folder
+		else{
+			var component = componentExist(filePath);
+			if(component){
+				componentUpdate(component, filePath, 'add');
+				componentRender(component);
+			}
+		}
 
 
-
-
-
-
-
-// module.exports = function(){
-
-
-	// // *************************************
-	// //  COMPONENT LIBRARY GENERATOR
-	// //	
-	// //	...
-	// //	
-	// // *************************************
-	
-	// // -------------------------------------
-	// //  Settings
-	// // -------------------------------------
-	// var location.src = 'src/components';
-	// var allowedFileExtensions = ['md', 'json', 'Nunjucks', 'njk'];
-
-
-	// // -------------------------------------
-	// //  Helper functions
-	// // -------------------------------------
-	// helpers.init();
-	// var objTypeString = Object.prototype.toString; // http://blog.niftysnippets.org/2010/09/say-what.html
-
-	// /**
-	//  * Initialize calls 3 basic functions
-	//  * a) save treeToJSON
-	//  * b) generateNavigation
-	//  * c) generateContent
-	//  */
-	// function initialization(){
-
-	// 	// -------------------------------------
-	// 	//  save fileTree to JSON
-	// 	// -------------------------------------
-	// 	console.info("Save Directory Tree in JSON");
-	// 	console.time("Timer");
-	// 	var fileTree = treeToJSON(location.src, allowedFileExtensions);
-	// 	// console.table(fileTree);
-	// 	// console.log(fileTree);
-	// 	console.timeEnd("Timer");
-
-
-	// 	// -------------------------------------
-	// 	//  generate navigation
-	// 	// -------------------------------------
-	// 	// console.info("Generate navigation");
-	// 	// console.time("Timer");
-	// 	// generateNavigation(fileTree);
-	// 	// // console.table(fileTree);
-	// 	// console.timeEnd("Timer");
-
-	// 	// -------------------------------------
-	// 	//  generate content
-	// 	// -------------------------------------
-	// 	// console.info("Generate content");
-	// 	// console.time("Timer");
-	// 	var content = generateContent(fileTree);
-	// 	// // console.table(fileTree);
-	// 	// console.timeEnd("Timer");
+		console.log(component);
+		console.table(components);
+	})
+	monitor.on("changed", function (filePath, curr, prev) {
+		console.warn('changed: '+filePath);
+		// if file changed - check if component exists
+		// if exist re-render component
+		var component = componentExist(filePath);
+		if(component){
+			componentRender(component);
+		}
 		
-
-	// 	// -------------------------------------
-	// 	//  filling template
-	// 	// -------------------------------------
-	// 	var placeholder = {
-	// 		styleguide: /\[\[\[styleguide-placeholder]]]/g
-	// 	},
-	// 		pageTemplate 	= 'src/styleguide/source/page.template',
-	// 		resultUrl 	 	= 'src/styleguide/index.njk',
-	// 		pageSource 		= fs.readFileSync(pageTemplate).toString(),
-	// 		newSource	 	= pageSource
-	// 							.replace(placeholder.styleguide, content);
-	// 							// .replace(placeholder.url, tmpl[index].replace(/\\/g,"/"))
-	// 	// console.log(content);
-	// 	// console.log(newSource);
-	// 	fs.writeFileSync(resultUrl, newSource);
-	// }
-
-
-	// /**
-	//  * Recursive function which walks the file-tree and saves it in to the JSON
-	//  * @param  {string} dir - recursion starting directory
-	//  * @param  {array} allowedFileExtensions - array of allowedFileExtensions file types (otherwise, ignore is more complicated)
-	//  * @return {JSON}  
-	//  */
-	// function treeToJSON(dir, allowedFileExtensions){
-	// 	var output = [];				// variable which later will be returned.
-	// 	var re = /(?:\.([^.]+))?$/; 	// get extension regexp 
-	// 	allowedFileExtensions.toLowerCase();   		// convert allowedFileExtensions filetypes to lowercase
-
-	// 	// Produce this JSON structure:
-	// 	// [{
-	// 	// 	"name": "january",
-	// 	// 	"path": "winter/january",
-	// 	// 	"type": "directory",
-	// 	// 	"children": [
-	// 	// 		{
-	// 	// 			"path": "winter/january/ski.png",
-	// 	// 			"name": "ski.png",
-	// 	// 			"type": "png"
-	// 	// 		},
-	// 	// 		{
-	// 	// 			"path": "winter/january/snowboard.jpg",
-	// 	// 			"name": "snowboard.jpg",
-	// 	// 			"type": "jpg"
-	// 	// 		}
-	// 	// 	]
-	// 	// }]
-	// 	fs.readdirSync(dir).filter(function(file) {
-	// 		var whitelisted = false;
-	// 		var item = {};
-	// 		item.name = file;
-	// 		item.path = path.join(dir, file);
-	// 		item.type = (re.exec(file)[1] == undefined)? 'folder': re.exec(file)[1].toLowerCase();
-
-	// 		var isDirectory = fs.statSync(item.path).isDirectory();
-	// 		if(isDirectory){
-	// 			// if directory call function on new path
-	// 			item.children = treeToJSON(item.path, allowedFileExtensions);
-	// 			// console.table(item.children);
-	// 			whitelisted = true;
-	// 		}
-	// 		else{
-	// 			// if file, make sure it's whitelisted
-	// 			if(objTypeString.call(allowedFileExtensions) == "[object Array]"){
-	// 				if(allowedFileExtensions.indexOf(item.type) > -1){
-	// 					whitelisted = true;
-	// 				}
-	// 			}
-	// 			else if(objTypeString.call(allowedFileExtensions) == "[object String]"){
-	// 				if(allowedFileExtensions==item.type){
-	// 					whitelisted = true;
-	// 				}
-	// 			}
-	// 		}
-	// 		if(whitelisted) output.push(item);
-	// 	});
-	// 	return output;
-	// }
-
-
-	// function generateNavigation(tree){
-
-	// }
-
-
-	// function generateContent(tree){
-	// 	var folderTemplate 	= 'src/styleguide/source/folder.template';
-	// 	var templateTemplate 	= 'src/styleguide/source/template.template';
-	// 	var outputUrl 			= 'src/styleguide/index.njk';
-	// 	var content 			= 'There are no components in components folder';
-	// 	var placeholder = {
-	// 		url: /\[\[\[template-url]]]/g,
-	// 		data: /\[\[\[template-data]]]/g,
-	// 		info: /\[\[\[folder-info]]]/g,
-	// 		tmpl: /\[\[\[folder-templates]]]/g
-	// 	};
-
-
-	// 	// -------------------------------------
-	// 	//  get array of components
-	// 	// -------------------------------------
-	// 	var components = JSONtoFile(tree);
-	// 	//console.table(components);
-	// 	// console.log(components.length);
-
-	// 	// -------------------------------------
-	// 	//  read template
-	// 	// -------------------------------------
-	// 	var folderContents = fs.readFileSync(folderTemplate).toString();
-	// 	var templateContents = fs.readFileSync(templateTemplate).toString();
-
-	// 	// -------------------------------------
-	// 	//  walk the tree
-	// 	// -------------------------------------
-	// 	if(components.length>0) content = "";
-	// 	for (var key in components) {
-	// 		if (components.hasOwnProperty(key)) {
-	// 			console.info("Building a folder");
-	// 			var tmplContent = '';
-	// 			var tmpl = components[key].tmpl;
-	// 			// var info = (objTypeString.call(components[key].info[0]) == "[object String]") ?
-	// 			// 	fs.readFileSync(components[key].info[0]).toString() : "";
-	// 			var info = (objTypeString.call(components[key].info[0]) == "[object String]") ?
-	// 				components[key].info[0] : "";
-	// 			var data = (objTypeString.call(components[key].data[0]) == "[object String]") ?
-	// 				fs.readFileSync(components[key].data[0]).toString() : "''";
-
-	// 			for (var index in tmpl) {
-	// 				if (tmpl.hasOwnProperty(index)) {
-	// 					// replace template & data placeholders
-	// 					tmplContent += templateContents
-	// 						.replace(placeholder.url, tmpl[index].replace(/\\/g,"/"))
-	// 						.replace(placeholder.data, data);
-	// 				}
-	// 			}
-
-	// 			// replace README & looping content placeholders
-	// 			content += folderContents
-	// 				.replace(placeholder.info, info.replace(/\\/g,"/"))
-	// 				.replace(placeholder.tmpl, tmplContent);
-	// 		}
-	// 	}
-
-	// 	//console.log(content);
-	// 	return content;
-	// }
-
-	// /**
-	//  * Recursive function which walks the JSON tree and
-	//  * returns an array of objects (components).
-	//  * Object consist of - template, data and info files.
-	//  * @param {json} items - file structure in JSON format
-	//  */
-	// function JSONtoFile(items){
-	// 	// console.info("----------- calling JSONtoFile -----------");
-	// 	var results = [],
-	// 		folder  = {},
-	// 		tmplExt = ['nunjucks', 'njk'],
-	// 		dataExt = ['json'],
-	// 		infoExt = ['markdown','md'];
-	// 	folder.tmpl = [];
-	// 	folder.data = [];
-	// 	folder.info = [];
-
-	// 	//http://stackoverflow.com/questions/684672/loop-through-javascript-object
-	// 	for (var key in items) {
-	// 		if (items.hasOwnProperty(key)) {
-	// 			if(items[key].type==='folder'){
-	// 				// console.info(objTypeString.call(items[key].children));
-	// 				if(objTypeString.call(items[key].children) != "[object Undefined]"){ //if has children
-	// 					var output = JSONtoFile(items[key].children);
-	// 					if(objTypeString.call(output)!="[object Undefined]"){
-	// 						// console.table(output);
-	// 						results = results.concat(output); 
-	// 					}
-	// 				}
-	// 			}
-	// 			else{
-	// 				if(tmplExt.indexOf(items[key].type) > -1){
-	// 					// console.info(items[key].type+" is a template!");
-	// 					folder.tmpl.push(items[key].path);
-	// 				}
-	// 				else if(dataExt.indexOf(items[key].type) > -1){
-	// 					// console.info(items[key].type+" is a data!");
-	// 					folder.data.push(items[key].path);
-	// 				}
-	// 				else if(infoExt.indexOf(items[key].type) > -1){
-	// 					// console.info(items[key].type+" is a documentation!");
-	// 					folder.info.push(items[key].path);
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-
-	// 	// merge arrays
-	// 	if(folder.tmpl.length > 0 || folder.data.length > 0 || folder.info.length > 0){
-	// 		// console.log(folder);
-	// 		results.push(folder)
-	// 	}
-
-	// 	// console.table(results);
-	// 	if (results.length > 0) return results;
-	// }
-
-	// initialization();
-// }
+	})
+	monitor.on("removed", function (filePath, stat) {
+		console.warn('removed: '+filePath);
+		// if template deleted - remove component
+		// if other file deleted - remove link & re-render component
+		var extension = path.extname(filePath).toLowerCase();
+		if(extension === extensions.template){
+			// folder can contain multiple tamplates
+			var component = componentExist(filePath);
+			if(component){
+				var count = templatesInFolder(component);
+				console.warn('templates in folder: '+count);
+				if (count === 0){
+					componentDelete(component);
+				}
+				else if(count > 0){
+					templateDelete(component);
+				}
+			}
+		}
+		else if(extension === ''){} // folder
+		else{
+			var component = componentExist(filePath);
+			if(component){
+				componentUpdate(component, filePath, 'rm');
+			}
+		}		 
+		 
+		console.table(components);
+	})
+// monitor.stop(); // Stop watching
+});
